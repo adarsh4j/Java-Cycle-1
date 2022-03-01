2a539d6b81c563a11e26b8d9c1dc6bb3bbd6a0cd
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-03-01 19:10:22 +0530

    Update Experiment9.java

--- Experiment9.java
+++ Experiment9.java
@@ -2,16 +2,27 @@ import java.util.*;
 
 class Experiment9{
 
-	public static void main(String[] args) {    
-  Scanner obj = new Scanner(System.in);
+	public static void main(String[] args) {      Scanner obj = new Scanner(System.in);
 
 	 System.out.println("Enter the number");
 
 	 int a = obj.nextInt();
 
-	 int i,temp=a,d;
+	 int i,temp=a,d,digit=0;
 
 	 double s=0;
+
+     while(a>0)
+
+        {
+
+            a = a/10;
+
+            digit++;
+
+        }
+
+        a=temp;
 
 	 while(a!=0)
 
@@ -19,7 +30,7 @@ class Experiment9{
 
 	 	d=a%10;
 
-	 	s=s+Math.pow(d,3);
+	 	s=s+Math.pow(d,digit);
 
 	 	a=a/10;
 
